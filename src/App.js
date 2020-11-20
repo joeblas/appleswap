ab7e84615d166a3d2d6d65e0dbe7bf900e52743e
Joe Gallegos <jbgallegos@cofisoftware.com>
2020-11-20 16:32:51 -0600

    Additional changes

--- src/App.js
+++ src/App.js
@@ -6,44 +6,35 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 
 import Col from 'react-bootstrap/Col';
-// import Card from 'react-bootstrap/Card';
-// import Button from 'react-bootstrap/Button';
-// import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
-// import { LinkContainer } from 'react-router-bootstrap';
 
 import './App.css';
 import useListings from './hooks';
 import { ListingCard } from './components';
 
-// const Home = () => <span>Home</span>;
-
-// const About = () => <span>About</span>;
-
-// const Users = () => <span>Users</span>;
-
 const App = () => {
   const { data: listings } = useListings();
   return (
     <MemoryRouter>
-      <Container className='p-3'>
-        <Jumbotron>
-          <h1>Apple Swap</h1>
-        </Jumbotron>
-        <Row>
-          {listings?.map((listing, index) => {
-          console.log("App -> listing", listing)
-            return (
-              <Col sm='12' className='mb-3' key={index}>
-                <ListingCard
-                  title={listing?.data?.title}
-                  description={listing?.data?.selftext_html}
-                  url={listing?.data?.url}
-                  created={listing?.data?.created_utc}
-                />
-              </Col>
-            );
-          })}
-        </Row>
+      <Container fluid className='bg-dark'>
+        <Container className='p-3'>
+          <Jumbotron>
+            <h1>Apple Swap</h1>
+            <p>Find people selling Apple products you want!</p>
+          </Jumbotron>
+          <Row>
+            {listings?.map(listing=> {
+              return (
+                <Col sm='12' className='mb-3' key={listing?.data?.id}>
+                  <ListingCard
+                    title={listing?.data?.title}
+                    description={listing?.data?.selftext_html}
+                    url={listing?.data?.url}
+                  />
+                </Col>
+              );
+            })}
+          </Row>
+        </Container>
       </Container>
     </MemoryRouter>
   );
--- src/components/ListingCard/ListingCard.js
+++ src/components/ListingCard/ListingCard.js
@@ -1,23 +1,16 @@
 import Card from 'react-bootstrap/Card';
 import parse, { domToReact } from 'html-react-parser';
+import Button from 'react-bootstrap/Button';
 
 const ListingCard = ({
   title = '',
   description = '',
   url = '',
-  created = '',
 }) => {
-  const date = new Date(created)
-  console.log("date", date)
   return (
     <Card style={{ width: '100%' }}>
-      <Card.Header>
-        {title}
-      </Card.Header>
+      <Card.Header>{title}</Card.Header>
       <Card.Body>
-        <Card.Subtitle className='mb-2 text-muted'>
-          {/* {Date.parse(created)} */}
-        </Card.Subtitle>
         {parse(description, {
           replace: (node) => {
             if (node?.name === 'p') {
@@ -27,7 +20,9 @@ const ListingCard = ({
         })}
       </Card.Body>
       <Card.Footer>
-        <Card.Link href={url}>Go to Post</Card.Link>
+        <Button variant='primary' href={url}>
+          More Info
+        </Button>
       </Card.Footer>
     </Card>
   );
